a1af10abe18bb99683da8aad60b1408983865b5f
Mr.sun <18273965210@163.com>
2020-10-21 14:56:13 +0800

    v1

---
+++ src/components/Notify.vue
@@ -0,0 +1,54 @@
+<!--
+ * @Author: sjm
+ * @Date: 2020-10-21 14:24:02
+ * @LastEditors: sjm
+ * @LastEditTime: 2020-10-21 14:45:02
+ * @FilePath: /v1/src/components/Notify.vue
+-->
+<template>
+    <div class='notify' v-if='isShow'>
+        <div class="title">{{title}}</div>
+        <div class="content">{{content}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            title:{
+                type:String,
+                default:''
+            },
+            content:{
+                type:String,
+                default:''
+            },
+            duration:{
+                type:Number,
+                default:2000
+            }
+        },
+        data(){
+            return {
+                isShow:false
+            }
+        },
+        methods: {
+            show() {
+                this.isShow = true
+                setTimeout(()=>{
+                    this.hide()
+
+                },this.duration)
+            },
+            hide(){
+                this.isShow = false
+                this.remove()
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/form/Minput.vue
@@ -0,0 +1,23 @@
+<template>
+    <input type="text" :value='value' @input='onInput' v-bind='$attrs'>
+</template>
+
+<script>
+    import emitter from '../../mixins/emitter'
+    export default {
+        mixins:[emitter],
+        props:{
+            value:{
+                type: String,
+                default: ''
+            }
+        },
+        methods: {
+            onInput(e) {
+                this.$emit('input',e.target.value)
+                this.dispatch('FormItem','validate')
+            }
+        },
+
+    }
+</script>
